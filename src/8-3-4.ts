5bae2ecf3d7796be0e607df83d7abbc0253fda26
kibutan <s1170230@gmail.com>
2022-09-09 16:49:30 +0900

    8-3-5

--- src/8-3-4.ts
+++ src/8-3-4.ts
@@ -6,3 +6,23 @@ const p = new Promise<number>((resolve) => {
 p.then((num) => {
   console.log(`結果は${num}`);
 });
+
+const sleep = (duration: number) => {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
+sleep(3000).then(() => {
+  console.log("3秒経過");
+});
+
+const sleepReject = (duration: number) => {
+  return new Promise<never>((resolve, reject) => {
+    setTimeout(reject, duration);
+  });
+};
+
+sleep(3000).then(() => {
+  console.log("失敗");
+});
---
+++ src/8-3-5.ts
@@ -0,0 +1,13 @@
+import { readFile } from "fs/promises";
+
+const p = Promise.all([
+  readFile("foo.txt", "utf-8"),
+  readFile("bar.txt", "utf-8"),
+  readFile("baz.txt", "utf-8"),
+]);
+
+p.then(([foo, bar, baz]) => {
+  console.log("foo.txt", foo);
+  console.log("bar.txt", bar);
+  console.log("baz.txt", baz);
+});
